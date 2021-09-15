f7444b96a6f04f185d106277cec0b55a98170038
mroldann <martinroldan@outlook.com>
2021-09-15 19:33:46 +0200

    Api key raise

--- utils/covalent.py
+++ utils/covalent.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 class Query:
     def __init__(self, api_key):
-        if not len(api_key) > 1:
+        if not api_key or len(api_key) > 1:
             raise 'Invalide Covalent key.'
         self.session = Session(
             server_url='https://api.covalenthq.com',
