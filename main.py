5a0cb5d1c4d9e4ff7307f95d56e46c36b7e27943
mroldann <martinroldan@outlook.com>
2021-09-15 12:37:18 +0200

    Added dotenv logic, arparse logic

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import argparse
 import pandas as pd
 from pathlib import Path
 import os 
-from utils._local_utils import load_env_vars
+from dotenv import load_dotenv
 from utils.the_graph import GraphQuery
 
-## Will only be used in local execution
-#ENV_VARS_PATH = 'env_vars/court_guardians.yaml'
-#ENV_VARS_PATH = 'env_vars/govern_daos.yaml'
-#ENV_VARS_PATH = 'env_vars/govern_executions.yaml'
+
+# Create the parser
+parser = argparse.ArgumentParser()
+# Add an arguments
+parser.add_argument('--local', type=bool, required=False, default=True)
+parser.add_argument('--testing', type=bool, required=False, default=True)
+parser.add_argument('--env_vars', type=str, required=False, default=None) # relative path
+args = parser.parse_args()
 
 def main(testing_mode=True):
-    #print(f'Setting env_vars: {env_vars_path}')
     print(f'TESTING_MODE: {testing_mode}')
-    #load_env_vars(env_vars_path) 
-    # Import GCP after setting env_vars (credentials)
-    from utils.bq import BQ_table
+    from utils.bq import BQ_table # Import GCP after setting env_vars (credentials)
 
     PROCESS_NAME = os.getenv('PROCESS_NAME')
     QUERY_PATH = Path(os.getenv('QUERY_DIR')).joinpath(PROCESS_NAME+'.graphql')
@@ -45,9 +46,16 @@ def main(testing_mode=True):
     errs = table.uplaoad_df_to_bq(df)
     if errs:
         return f'Execution ended with {len(errs)} errors. Check Logging.'
-    
-    
     return f'Execution succeded. Table: {table.table_id}. Shape: {df.shape}'
 
+print('LOCAL_MODE: ', args.local)
+if args.local:
+    # Set local env-vars
+    _ENV_VARS_PATH = './env_vars/court_guardians.env'
+    print(args)
+    ENV_VARS_PATH = args.env_vars if args.env_vars != None else _ENV_VARS_PATH
+    print('ENV_VARS_PATH:', ENV_VARS_PATH)
+    load_dotenv(dotenv_path=ENV_VARS_PATH, override=True)
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('LOCAL_GOOGLE_APPLICATION_CREDENTIALS')    
 
-print(main(testing_mode=True))
+print(main(testing_mode=args.testing))
