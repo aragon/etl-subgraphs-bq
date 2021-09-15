fb37a633f41f760925b781a3821ea9446f10171b
mroldann <martinroldan@outlook.com>
2021-09-15 13:11:51 +0200

    Improved argparse logic

--- main.py
+++ main.py
@@ -8,14 +8,22 @@ from utils.the_graph import GraphQuery
 
 # Create the parser
 parser = argparse.ArgumentParser()
-# Add an arguments
-parser.add_argument('--local', type=bool, required=False, default=True)
-parser.add_argument('--testing', type=bool, required=False, default=True)
+# Add argument to differenciate between local executions
+parser.add_argument('--local', dest='local', action='store_true')
+parser.add_argument('--no-local', dest='local', action='store_false')
+parser.set_defaults(local=True)
+
+# Add argument to point to testing tables
+parser.add_argument('--testing', dest='testing', action='store_true')
+parser.add_argument('--no-testing', dest='testing', action='store_false')
+parser.set_defaults(testing=True)
+
+# Add argument to define env_vars to use
 parser.add_argument('--env_vars', type=str, required=False, default=None) # relative path
 args = parser.parse_args()
+print("args: ", args)
 
 def main(testing_mode=True):
-    print(f'TESTING_MODE: {testing_mode}')
     from utils.bq import BQ_table # Import GCP after setting env_vars (credentials)
 
     PROCESS_NAME = os.getenv('PROCESS_NAME')
@@ -48,14 +56,14 @@ def main(testing_mode=True):
         return f'Execution ended with {len(errs)} errors. Check Logging.'
     return f'Execution succeded. Table: {table.table_id}. Shape: {df.shape}'
 
-print('LOCAL_MODE: ', args.local)
 if args.local:
     # Set local env-vars
     _ENV_VARS_PATH = './env_vars/court_guardians.env'
-    print(args)
     ENV_VARS_PATH = args.env_vars if args.env_vars != None else _ENV_VARS_PATH
     print('ENV_VARS_PATH:', ENV_VARS_PATH)
     load_dotenv(dotenv_path=ENV_VARS_PATH, override=True)
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('LOCAL_GOOGLE_APPLICATION_CREDENTIALS')    
 
+
 print(main(testing_mode=args.testing))
+print('GOOGLE_APPLICATION_CREDENTIALS: ', os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
