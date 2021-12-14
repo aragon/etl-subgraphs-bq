fc43ab4d4031cc637055a48997a15c9ca4e5fa75
mroldann <32395775+mroldann@users.noreply.github.com>
2021-12-14 00:14:55 -0300

    finished votes

--- main_snapshot.py
+++ main_snapshot.py
@@ -10,14 +10,19 @@ import re
 def main(testing_mode=True):
     from utils.bq import BQ_table # Import GCP after setting env_vars (credentials)
 
-    SPACES_QUERY_PATH = Path(os.getenv('SPACES_QUERY_PATH'))
-    PROPOSALS_QUERY_PATH = Path(os.getenv('PROPOSALS_QUERY_PATH'))
     API_URL = os.getenv('API_URL')
     BQ_DATASET = os.getenv('BQ_DATASET')
+
+    SPACES_QUERY_PATH = Path(os.getenv('SPACES_QUERY_PATH'))
+    PROPOSALS_QUERY_PATH = Path(os.getenv('PROPOSALS_QUERY_PATH'))
+    VOTES_QUERY_PATH = Path(os.getenv('VOTES_QUERY_PATH'))
     SPACES_BQ_TABLE = os.getenv('SPACES_BQ_TABLE')
     PROPOSALS_BQ_TABLE = os.getenv('PROPOSALS_BQ_TABLE')
     PROPOSALS_DATE_RANGE_ATTR = os.getenv('PROPOSALS_DATE_RANGE_ATTR')
     PROPOSALS_DATE_RANGE_COL = os.getenv('PROPOSALS_DATE_RANGE_COL')
+    VOTES_BQ_TABLE = os.getenv('VOTES_BQ_TABLE')
+    VOTES_DATE_RANGE_ATTR = os.getenv('VOTES_DATE_RANGE_ATTR')
+    VOTES_DATE_RANGE_COL = os.getenv('VOTES_DATE_RANGE_COL')
     
     if testing_mode: SPACES_BQ_TABLE = "test_" + SPACES_BQ_TABLE
 
@@ -65,7 +70,6 @@ def main(testing_mode=True):
     proposals_table = BQ_table(BQ_DATASET, PROPOSALS_BQ_TABLE)
 
     last_update = 0
-    df_proposals_base = pd.DataFrame()
     if proposals_table.exists:
             last_update_tmp = proposals_table.get_last_block(PROPOSALS_DATE_RANGE_COL)
             last_update = last_update_tmp if last_update_tmp != None else last_update
@@ -104,10 +108,48 @@ def main(testing_mode=True):
             print(f'Proposals: Execution ended with {len(errs)} errors. Check Logging.')
         print(f"""Proposals: Table: {proposals_table.table_id}. Shape: {df_proposals_new.shape}""")
 
+    # Load Votes
+    print("Votes: quering started.")
+    votes_table = BQ_table(BQ_DATASET, VOTES_BQ_TABLE)
 
-    
-    
-    return 'Finished exectution!'
+    last_update = 0
+    if votes_table.exists:
+            last_update_tmp = votes_table.get_last_block(VOTES_DATE_RANGE_COL)
+            last_update = last_update_tmp if last_update_tmp != None else last_update
+            print(f"Votes: last update at database:{last_update}")
+
+    query = GraphQuery(
+        query_path=VOTES_QUERY_PATH,
+        api_url=API_URL,
+        query_first='1000',
+        query_skip='0',
+        gt_statement=VOTES_DATE_RANGE_ATTR,
+        gt_value=int(last_update)+1
+        )
+
+    data = query.post(
+        paginate=True,
+        date_filter=True)
+
+    df_votes_new = pd.DataFrame(data)
+    lambda_replace = lambda x: x.replace('\n',"").replace('**',"")
+
+    print(f"Votes: new fields to add {df_votes_new.shape[0]}")
+    lastt = int(df_votes_new[VOTES_DATE_RANGE_COL].max())
+    print(f'Votes: Last update to upload: {lastt}')
+
+    if not votes_table.exists:
+        votes_table.create_table_from_df(
+            df=df_votes_new,
+            partitioned_day=True)
+
+    if not df_votes_new.empty:
+        errs = votes_table.uplaoad_df_to_bq(df_votes_new)
+        if errs:
+            print(f'Votes: Execution ended with {len(errs)} errors. Check Logging.')
+        print(f"""Votes: Table: {votes_table.table_id}. Shape: {df_votes_new.shape}""")
+
+    return lastt
 
 
 _ENV_VARS_PATH = './env_vars/snapshot.env'
